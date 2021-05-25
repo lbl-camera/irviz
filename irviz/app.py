c90713f8eddfbad98c861a48cfe322a4fd9ef33e
Ian Humphrey <ian.r.humphrey@gmail.com>
2021-05-25 15:22:24 -0700

    ticks, margins, todos

--- irviz/app.py
+++ irviz/app.py
@@ -3,6 +3,15 @@ import dash_html_components as html
 from dash_slicer import VolumeSlicer
 import h5py as h5
 
+# TODO:
+#  orthogonal views
+#    - 1 2D orthogonal view
+#    - 1 1D orthogonal view
+#  callback implemented on pixel selection (clicked)
+#    - update orthogonal views
+#    - display I, coords, etc.
+#  ***
+#  Understand the slicer module
 
 def open_ir_file(h5_file):
     f = h5.File(h5_file, 'r')
@@ -13,6 +22,22 @@ app = dash.Dash(__name__, update_title=None)
 
 volume = open_ir_file('/home/ihumphrey/Dev/xicam/data/ir_stxm.h5')
 slicer = VolumeSlicer(app, volume)
+
+slicer.graph.figure.update_xaxes(title_text='blah',
+                                 showgrid=True,
+                                 showticklabels=True,
+                                 tickmode='linear',
+                                 tick0=0,
+                                 dtick=1)
+slicer.graph.figure.update_yaxes(title_text='y axis',
+                                 showgrid=True,
+                                 showticklabels=True,
+                                 tick0=0,
+                                 dtick=1)
+slicer.graph.figure.update_layout(title="Title",
+                                  xaxis_title="X Axis",
+                                  yaxis_title="Y Axis",
+                                  margin=None)  # {"l": 0, "r": 0, "b": 30, "t": 0, "pad": 10})
 
 app.layout = html.Div(children=[slicer.graph, slicer.slider, *slicer.stores])
 
