0803de9204f6b7facb1ef8787f26d373031b8ea0
Ian Humphrey <ian.r.humphrey@gmail.com>
2021-05-25 15:53:51 -0700

    show coords and I when pixel is clicked

--- irviz/app.py
+++ irviz/app.py
@@ -1,7 +1,11 @@
+import json
+
 import dash
 import dash_html_components as html
+from dash.dependencies import Input, Output
 from dash_slicer import VolumeSlicer
 import h5py as h5
+
 
 # TODO:
 #  orthogonal views
@@ -11,6 +15,7 @@ import h5py as h5
 #    - update orthogonal views
 #    - display I, coords, etc.
 #  ***
+#  Add vertical spanner (to show how orthogonal view is being sliced)
 #  Understand the slicer module
 
 def open_ir_file(h5_file):
@@ -39,7 +44,30 @@ slicer.graph.figure.update_layout(title="Title",
                                   yaxis_title="Y Axis",
                                   margin=None)  # {"l": 0, "r": 0, "b": 30, "t": 0, "pad": 10})
 
-app.layout = html.Div(children=[slicer.graph, slicer.slider, *slicer.stores])
+app.layout = html.Div(children=[
+    slicer.graph,
+    html.Div(id='pixel_info'),
+    slicer.slider,
+    html.Div(id='debug_pixel_info'),
+    *slicer.stores
+])
+
+
+@app.callback(Output('pixel_info', 'children'),
+              [Input(slicer.graph.id, 'clickData')])
+def update_pixel_info(click_data):
+    if click_data is not None:
+        pixel = click_data['points'][0]
+        x = pixel['x']
+        y = pixel['y']
+        z = pixel['z']['0']  # R of RGBA z value (RGB same since greyscale)
+        return f"x: {x}, y: {y}, I: {z}"
+    return "x: , y: , I: "
+
+# @app.callback(Output('debug_pixel_info', 'children'),
+#               [Input(slicer.graph.id, 'clickData')])
+# def debug_update_pixel_info(state):
+#     return json.dumps(state, indent=4)
 
 
 if __name__ == "__main__":
