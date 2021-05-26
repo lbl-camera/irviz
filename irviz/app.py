590083a618825742b4db30472b5ac90ca74f643b
Ian Humphrey <ian.r.humphrey@gmail.com>
2021-05-25 17:54:27 -0700

    add y/x slicing, spectral plotting

--- irviz/app.py
+++ irviz/app.py
@@ -1,10 +1,11 @@
-import json
-
 import dash
+import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 from dash_slicer import VolumeSlicer
 import h5py as h5
+import plotly.graph_objects as go
+import numpy as np
 
 
 # TODO:
@@ -26,35 +27,77 @@ def open_ir_file(h5_file):
 app = dash.Dash(__name__, update_title=None)
 
 volume = open_ir_file('/home/ihumphrey/Dev/xicam/data/ir_stxm.h5')
-slicer = VolumeSlicer(app, volume)
+z_slicer = VolumeSlicer(app, volume, axis=0)
+y_slicer = VolumeSlicer(app, volume, axis=1)
+x_slicer = VolumeSlicer(app, volume, axis=2)
 
-slicer.graph.figure.update_xaxes(title_text='blah',
-                                 showgrid=True,
-                                 showticklabels=True,
-                                 tickmode='linear',
-                                 tick0=0,
-                                 dtick=1)
-slicer.graph.figure.update_yaxes(title_text='y axis',
-                                 showgrid=True,
-                                 showticklabels=True,
-                                 tick0=0,
-                                 dtick=1)
-slicer.graph.figure.update_layout(title="Title",
-                                  xaxis_title="X Axis",
-                                  yaxis_title="Y Axis",
-                                  margin=None)  # {"l": 0, "r": 0, "b": 30, "t": 0, "pad": 10})
+axes_opts = {'showgrid': True, 'showticklabels': True, 'tick0': 0, 'dtick': 1}
 
-app.layout = html.Div(children=[
-    slicer.graph,
+z_slicer.graph.figure.update_layout(title="Title",
+                                    xaxis_title="X Axis",
+                                    yaxis_title="Y Axis",
+                                    xaxis=axes_opts,
+                                    yaxis=axes_opts,
+                                    margin=None)  # {"l": 0, "r": 0, "b": 30, "t": 0, "pad": 10})
+y_slicer.graph.figure.update_layout(title="Y Slice",
+                                    xaxis_title="X",
+                                    yaxis_title="Z",
+                                    xaxis_showticklabels=True,
+                                    yaxis_showticklabels=True,
+                                    xaxis_showgrid=True,
+                                    yaxis_showgrid=True,
+                                    margin=None)
+x_slicer.graph.figure.update_layout(title="X Slice",
+                                    xaxis_title="Y",
+                                    yaxis_title="Z",
+                                    xaxis_showticklabels=True,
+                                    yaxis_showticklabels=True,
+                                    xaxis_showgrid=True,
+                                    yaxis_showgrid=True,
+                                    margin=None)
+
+z_view = html.Div([
+    z_slicer.graph,
     html.Div(id='pixel_info'),
-    slicer.slider,
+    z_slicer.slider,
     html.Div(id='debug_pixel_info'),
-    *slicer.stores
+    *z_slicer.stores
+])
+y_view = html.Div([
+    y_slicer.graph,
+    y_slicer.slider,
+    *y_slicer.stores
+])
+x_view = html.Div([
+    x_slicer.graph,
+    x_slicer.slider,
+    *x_slicer.stores
+])
+
+spectra_plot = html.Div([
+    dcc.Graph(id='spectra_plot')
+])
+
+app.layout = html.Div([
+    html.Div(
+        style={
+            'display': 'grid',
+            'gridTemplateColumns': '40% 30% 30%',
+            'gridTemplateRows': 'auto auto'
+        },
+        children=[
+            z_view,
+            y_view,
+            x_view
+        ]),
+    html.Div(
+        spectra_plot
+    )
 ])
 
 
 @app.callback(Output('pixel_info', 'children'),
-              [Input(slicer.graph.id, 'clickData')])
+              [Input(z_slicer.graph.id, 'clickData')])
 def update_pixel_info(click_data):
     if click_data is not None:
         pixel = click_data['points'][0]
@@ -64,10 +107,28 @@ def update_pixel_info(click_data):
         return f"x: {x}, y: {y}, I: {z}"
     return "x: , y: , I: "
 
-# @app.callback(Output('debug_pixel_info', 'children'),
-#               [Input(slicer.graph.id, 'clickData')])
-# def debug_update_pixel_info(state):
-#     return json.dumps(state, indent=4)
+
+@app.callback(Output('spectra_plot', 'figure'),
+              [Input(z_slicer.state.id, 'data'),
+               Input(y_slicer.state.id, 'data'),
+               Input(x_slicer.state.id, 'data')])
+def do_thing(z_data, y_data, x_data):
+    global volume
+    if z_data and y_data and x_data:
+        y_index = y_data['index']
+        x_index = x_data['index']
+        y = volume[:, y_index, x_index]
+        x = np.arange(0, volume.shape[0])
+    else:
+        y_index = int(volume.shape[1] / 2)
+        x_index = int(volume.shape[2] / 2)
+        y = volume[:, y_index, x_index]
+        x = np.arange(0, volume.shape[0])
+
+    fig = go.Figure(data={'type': 'scatter', 'x': x, 'y': y})
+    fig.update_layout(title=f'Spectra Intensities @ (x: {x_index}, y: {y_index})',
+                      xaxis_title="Spectra",
+                      yaxis_title="Intensity")
 
 
 if __name__ == "__main__":
