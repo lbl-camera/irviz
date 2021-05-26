45b0e83f3b33972f63cc1eee79bc266a8e67198f
Ian Humphrey <ian.r.humphrey@gmail.com>
2021-05-25 18:10:37 -0700

    small fixes, add basic comments, add callback protection for slider states

--- irviz/app.py
+++ irviz/app.py
@@ -2,22 +2,16 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 from dash_slicer import VolumeSlicer
 import h5py as h5
 import plotly.graph_objects as go
 import numpy as np
 
-
-# TODO:
-#  orthogonal views
-#    - 1 2D orthogonal view
-#    - 1 1D orthogonal view
-#  callback implemented on pixel selection (clicked)
-#    - update orthogonal views
-#    - display I, coords, etc.
 #  ***
 #  Add vertical spanner (to show how orthogonal view is being sliced)
 #  Understand the slicer module
+
 
 def open_ir_file(h5_file):
     f = h5.File(h5_file, 'r')
@@ -26,13 +20,14 @@ def open_ir_file(h5_file):
 
 app = dash.Dash(__name__, update_title=None)
 
+# Create different 2D volume slicers, one for each axis (z, y, x)
 volume = open_ir_file('/home/ihumphrey/Dev/xicam/data/ir_stxm.h5')
 z_slicer = VolumeSlicer(app, volume, axis=0)
 y_slicer = VolumeSlicer(app, volume, axis=1)
 x_slicer = VolumeSlicer(app, volume, axis=2)
 
+# Override some of the layout changes that VolumeSlicer does
 axes_opts = {'showgrid': True, 'showticklabels': True, 'tick0': 0, 'dtick': 1}
-
 z_slicer.graph.figure.update_layout(title="Title",
                                     xaxis_title="X Axis",
                                     yaxis_title="Y Axis",
@@ -56,6 +51,7 @@ x_slicer.graph.figure.update_layout(title="X Slice",
                                     yaxis_showgrid=True,
                                     margin=None)
 
+# Describe our html elements for each slicer "view"
 z_view = html.Div([
     z_slicer.graph,
     html.Div(id='pixel_info'),
@@ -73,7 +69,7 @@ x_view = html.Div([
     x_slicer.slider,
     *x_slicer.stores
 ])
-
+# Describe html layout for spectral plot
 spectra_plot = html.Div([
     dcc.Graph(id='spectra_plot')
 ])
@@ -113,22 +109,19 @@ def update_pixel_info(click_data):
                Input(y_slicer.state.id, 'data'),
                Input(x_slicer.state.id, 'data')])
 def do_thing(z_data, y_data, x_data):
-    global volume
-    if z_data and y_data and x_data:
-        y_index = y_data['index']
-        x_index = x_data['index']
-        y = volume[:, y_index, x_index]
-        x = np.arange(0, volume.shape[0])
-    else:
-        y_index = int(volume.shape[1] / 2)
-        x_index = int(volume.shape[2] / 2)
-        y = volume[:, y_index, x_index]
-        x = np.arange(0, volume.shape[0])
+    # We need all the slicer state data ready; otherwise, don't update any Graph objects
+    if z_data is None or y_data is None or x_data is None:
+        raise PreventUpdate
 
+    y_index = y_data['index']
+    x_index = x_data['index']
+    y = volume[:, y_index, x_index]
+    x = np.arange(0, volume.shape[0])
     fig = go.Figure(data={'type': 'scatter', 'x': x, 'y': y})
     fig.update_layout(title=f'Spectra Intensities @ (x: {x_index}, y: {y_index})',
                       xaxis_title="Spectra",
                       yaxis_title="Intensity")
+    return fig
 
 
 if __name__ == "__main__":
