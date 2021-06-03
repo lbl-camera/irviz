6b8884e15b43c956385f29df6c4df91e927c034d
Ian Humphrey <ian.r.humphrey@gmail.com>
2021-06-03 11:37:51 -0700

    implement slicers on image view

--- irviz/graphs.py
+++ irviz/graphs.py
@@ -16,27 +16,58 @@ class SpectraPlotGraph(dcc.Graph):
         self._data = data
         self._parent = parent
 
+        y = np.asarray(self._data[:, 0, 0])
+        x = np.arange(0, self._data.shape[0])
+        self._plot = go.Scatter(x=x, y=y)
+        # x coords positioned relative to the x-axis values
+        # y coords positioned according to the plot height (0 = bottom, 1.0 = top)
+        self._energy_line = go.layout.Shape(type='line',
+                                       # width=3,
+                                      xref='x',
+                                      yref='paper',
+                                       x0=0,
+                                       x1=0,
+                                       y0=0,
+                                       y1=1)
+
+        self._update_figure()
+
         super(SpectraPlotGraph, self).__init__(id=self._id())
 
     def register_callbacks(self):
         self._parent._app.callback(
             Output(self.id, 'figure'),
-            Input(self._parent.slice_graph.id, 'clickData'))(self._update_figure)
+            Input(self._parent.slice_graph.id, 'clickData'),
+            Input(self.id, 'clickData')
+        )(self._show_plot)
 
-    def _update_figure(self, click_data):
-        if click_data is None:
-            raise PreventUpdate
-
-        y_index = click_data["points"][0]["y"]
-        x_index = click_data["points"][0]["x"]
-
-        y = np.asarray(self._data[:, y_index, x_index])
-        x = np.arange(0, self._data.shape[0])
-        fig = go.Figure(data={'type': 'scatter', 'x': x, 'y': y})
-        fig.update_layout(title=f'Spectra Intensities @ (x: {x_index}, y: {y_index})',
+    def _update_figure(self):
+        fig = go.Figure(self._plot)
+        fig.update_layout(title=f'Spectra Intensities',
                           xaxis_title="Spectra",
                           yaxis_title="Intensity")
+        fig.add_shape(self._energy_line)
         return fig
+
+    def _show_plot(self, slice_graph_click_data, self_click_data):
+        if slice_graph_click_data is None and self_click_data is None:
+            raise PreventUpdate
+
+        y_index = slice_graph_click_data["points"][0]["y"]
+        x_index = slice_graph_click_data["points"][0]["x"]
+
+        self._plot.y = np.asarray(self._data[:, y_index, x_index])
+        self._plot.x = np.arange(0, self._data.shape[0])
+
+        if self_click_data is None:
+            energy_index = 0
+        else:
+            energy_index = self_click_data["points"][0]["x"]
+
+        self._energy_line.x0 = energy_index
+        self._energy_line.x1 = energy_index
+
+        return self._update_figure()
 
     def _id(self):
         return f'spectraplot_{self._counter}'
@@ -65,14 +96,26 @@ class SliceGraph(dcc.Graph):
         self._image = go.Heatmap(z=np.asarray(self._data[0]))
         self._h_line = go.layout.Shape(type='line',
                                        # width=3,
+                                       xref='paper',
+                                       yref='y',
                                        x0=0,
-                                       x1=self._data.shape[2] - 1,
-                                       y0=self._data.shape[1] / 2,
-                                       y1=self._data.shape[1] / 2)
+                                       x1=1,
+                                       y0=self._data.shape[2] / 2,
+                                       y1=self._data.shape[2] / 2)
+        self._v_line = go.layout.Shape(type='line',
+                                       xref='x',
+                                       yref='paper',
+                                       x0=self._data.shape[1] / 2,
+                                       x1=self._data.shape[1] / 2,
+                                       y0=0,
+                                       y1=1)
 
         figure = self._update_figure()
         super(SliceGraph, self).__init__(figure=figure,
-                                         id=f'sliceview_{self._counter}')
+                                         id=self._id())
+
+    def _id(self):
+        return f'slicegraph_{self._counter}'
 
     def register_callbacks(self):
         # Set up callbacks
@@ -82,27 +125,41 @@ class SliceGraph(dcc.Graph):
         #     we need to update the internal Figure for this Graph
         self._parent._app.callback(
             Output(self.id, 'figure'),
-            Input(self._parent.spectra_graph.id, 'clickData')
+            Input(self._parent.spectra_graph.id, 'clickData'),
+            Input(self.id, 'clickData')
         )(self.show_slice)
 
     def _update_figure(self):
         figure = go.Figure([self._image])
         figure.add_shape(self._h_line)
+        figure.add_shape(self._v_line)
         return figure
 
-    def show_slice(self, click_data):
+    def show_slice(self, spectra_graph_click_data, self_click_data):
         """Show a 2D slice at a specific energy defined in click data.
 
         Parameters
         ----------
-        click_data : dict
+        spectra_graph_click_data : dict
             Dictionary that contains point info from where the input Graph was clicked.
         """
-        if click_data is None:
+        if spectra_graph_click_data is None and self_click_data is None:
             raise PreventUpdate
 
-        energy_index = click_data["points"][0]["x"]
+        if spectra_graph_click_data is None:
+            energy_index = 0
+        else:
+            energy_index = spectra_graph_click_data["points"][0]["x"]
+
         self._image.z = numpy.asarray(self._data[energy_index])
+
+        if self_click_data:
+            y_index = self_click_data["points"][0]["y"]
+            x_index = self_click_data["points"][0]["x"]
+            self._h_line.y0 = y_index
+            self._h_line.y1 = y_index
+            self._v_line.x0 = x_index
+            self._v_line.x1 = x_index
         # Need to update our figure again when we update the traces
         return self._update_figure()
 
