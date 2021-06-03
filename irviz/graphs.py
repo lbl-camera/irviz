248c6e3691db48138a4382fe6041bf65609c727b
Ian Humphrey <ian.r.humphrey@gmail.com>
2021-06-03 12:17:44 -0700

    handle multiple inputs better in callbacks

--- irviz/graphs.py
+++ irviz/graphs.py
@@ -16,17 +16,26 @@ class SpectraPlotGraph(dcc.Graph):
         self._data = data
         self._parent = parent
 
-        y = np.asarray(self._data[:, 0, 0])
+        # Define starting point for energy index (for the slicer line trace)
+        default_energy_index = (self._data.shape[0] - 1) // 2
+
+        # Cache the x,y coordinates for slicing into the data for the plot
+        #  (default to middle x,y)
+        y_index = (self._data.shape[1] - 1) // 2
+        x_index = (self._data.shape[2] - 1) // 2
+
+        y = np.asarray(self._data[:, y_index, x_index])
         x = np.arange(0, self._data.shape[0])
         self._plot = go.Scatter(x=x, y=y)
+
         # x coords positioned relative to the x-axis values
         # y coords positioned according to the plot height (0 = bottom, 1.0 = top)
         self._energy_line = go.layout.Shape(type='line',
                                        # width=3,
                                       xref='x',
                                       yref='paper',
-                                       x0=0,
-                                       x1=0,
+                                       x0=default_energy_index,
+                                       x1=default_energy_index,
                                        y0=0,
                                        y1=1)
 
@@ -54,24 +63,24 @@ class SpectraPlotGraph(dcc.Graph):
         if slice_graph_click_data is None and self_click_data is None:
             raise PreventUpdate
 
-        y_index = slice_graph_click_data["points"][0]["y"]
-        x_index = slice_graph_click_data["points"][0]["x"]
+        # When the slice graph is clicked, update plot with the clicked x,y coord
+        if slice_graph_click_data is not None:
+            y_index = slice_graph_click_data["points"][0]["y"]
+            x_index = slice_graph_click_data["points"][0]["x"]
+            self._plot.y = np.asarray(self._data[:, y_index, x_index])
+            self._plot.x = np.arange(0, self._data.shape[0])
 
-        self._plot.y = np.asarray(self._data[:, y_index, x_index])
-        self._plot.x = np.arange(0, self._data.shape[0])
-
-        if self_click_data is None:
-            energy_index = 0
-        else:
+        # When this SpectraGraph itself is clicked, update the energy slicer line
+        if self_click_data is not None:
             energy_index = self_click_data["points"][0]["x"]
-
-        self._energy_line.x0 = energy_index
-        self._energy_line.x1 = energy_index
+            self._energy_line.x0 = energy_index
+            self._energy_line.x1 = energy_index
 
         return self._update_figure()
 
     def _id(self):
         return f'spectraplot_{self._counter}'
+
 
 class SliceGraph(dcc.Graph):
     """Dash Graph for viewing 2D slices of 3D data.
@@ -93,21 +102,23 @@ class SliceGraph(dcc.Graph):
         self._data = data
         self._parent = parent
 
+        default_energy_index = (self._data.shape[0] - 1) // 2
+
         # Create traces (i.e. 'glyphs') that will comprise a plotly Figure
-        self._image = go.Heatmap(z=np.asarray(self._data[0]))
+        self._image = go.Heatmap(z=np.asarray(self._data[default_energy_index]))
         self._h_line = go.layout.Shape(type='line',
                                        # width=3,
                                        xref='paper',
                                        yref='y',
                                        x0=0,
                                        x1=1,
-                                       y0=0,  # (self._data.shape[2] - 1) // 2,
-                                       y1=0)  # (self._data.shape[2] - 1) // 2)
+                                       y0=(self._data.shape[2] - 1) // 2,
+                                       y1=(self._data.shape[2] - 1) // 2)
         self._v_line = go.layout.Shape(type='line',
                                        xref='x',
                                        yref='paper',
-                                       x0=0,  # (self._data.shape[1] - 1) // 2,
-                                       x1=0,  # (self._data.shape[1] - 1) // 2,
+                                       x0=(self._data.shape[1] - 1) // 2,
+                                       x1=(self._data.shape[1] - 1) // 2,
                                        y0=0,
                                        y1=1)
 
@@ -147,13 +158,12 @@ class SliceGraph(dcc.Graph):
         if spectra_graph_click_data is None and self_click_data is None:
             raise PreventUpdate
 
-        if spectra_graph_click_data is None:
-            energy_index = 0
-        else:
+        # When the spectra graph is clicked, update image slicing
+        if spectra_graph_click_data is not None:
             energy_index = spectra_graph_click_data["points"][0]["x"]
+            self._image.z = numpy.asarray(self._data[energy_index])
 
-        self._image.z = numpy.asarray(self._data[energy_index])
-
+        # When this SliceGraph itself is clicked, update its x,y slicer lines
         if self_click_data:
             y_index = self_click_data["points"][0]["y"]
             x_index = self_click_data["points"][0]["x"]
@@ -161,8 +171,6 @@ class SliceGraph(dcc.Graph):
             self._h_line.y1 = y_index
             self._v_line.x0 = x_index
             self._v_line.x1 = x_index
+
         # Need to update our figure again when we update the traces
         return self._update_figure()
-
-    def show_location(self, location):
-        ...
