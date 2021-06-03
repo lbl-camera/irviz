d0d0304104552ee4c21e5e4c2c13cf088e2ab7d7
ronpandolfi <ronpandolfi@gmail.com>
2021-06-03 13:28:24 -0700

    Add app template; add bounds support (WIP); use sources for image glyphs; lasso selection

--- irviz/main.py
+++ irviz/main.py
@@ -13,12 +13,15 @@ import sklearn.decomposition
 def open_ir_file(h5_file):
     f = h5.File(h5_file, 'r')
     data = f['irmap']['DATA']['data']
-    return da.from_array(data)
+    bounds_grid = f['irmap']['DATA']['energy'][:], f['irmap']['DATA']['sample_y'][:], f['irmap']['DATA']['sample_x'][:]
+    bounds = list(map(lambda grid: (grid.min(), grid.max()-grid.min()), bounds_grid))
+    print(bounds)
+    return da.from_array(data), bounds
 
-data = open_ir_file('C:\\Users\\LBL\\PycharmProjects\\Xi-CAM\\TestData\\STXM\\nexus\\ir_stxm.h5')
+data, bounds = open_ir_file('C:\\Users\\LBL\\PycharmProjects\\Xi-CAM\\TestData\\STXM\\nexus\\ir_stxm.h5')
 model = sklearn.decomposition.PCA(n_components=3)
 decomposition = model.fit_transform(data.transpose(1,2,0).reshape(-1, data.shape[0])).T.reshape(-1, *data.shape[1:])
 
-viewer = Viewer(data, decomposition)
+viewer = Viewer(data, decomposition=decomposition) # bounds=bounds
 curdoc().add_root(viewer.widget)
 curdoc().theme = 'dark_minimal'
--- irviz/viewer.py
+++ irviz/viewer.py
@@ -3,43 +3,61 @@ from typing import List
 from bokeh.application import Application
 from bokeh.application.handlers import FunctionHandler
 from bokeh.models.widgets import Slider, DataTable, Div, Tabs, Panel, Dropdown, RadioButtonGroup, Toggle
-from bokeh.models import Span, GridBox, ProxyToolbar, ToolbarBox
+from bokeh.models import Span, GridBox, ProxyToolbar, ToolbarBox, glyphs
 from bokeh.io import show, output_notebook
 from bokeh.plotting import figure, curdoc, ColumnDataSource
 from bokeh.layouts import column, row, layout, gridplot, grid
 from bokeh.events import Tap
 import numpy as np
-import h5py as h5
-from dask import array as da
+
+# TODO:
+# Bind fig.on_event(SelectionGeometry, ...) to capture lasso selection on images then select appro. points in pair plot
 
 
 class Viewer:
 
-    def __init__(self, data, decompostion=None, location: List[int] = None):
-
+    def __init__(self, data, decomposition=None, bounds=None, location: List[int] = None):
+        # State stash
         self.location = location or list(np.asarray(data.shape)/2)
         self.data = data
-        self.decomposition = decompostion
+        self.decomposition = decomposition
+        self.bounds = bounds
+        if self.bounds is None:
+            self.bounds = list(zip([0, 0, 0], self.data.shape))
 
         # Build figures
-        self.main_figure = figure()
+        self.main_figure = figure(title='IR Spectral Map')
         self.main_figure.xaxis.axis_label = 'X'
         self.main_figure.yaxis.axis_label = 'Y'
-        self.right_figure = figure()
+        self.right_figure = figure(title='Orthogonal Slice (X)', visible=False)
         self.right_figure.xaxis.axis_label = 'E'
         self.right_figure.yaxis.axis_label = 'Y'
-        self.top_figure = figure()
+        self.top_figure = figure(title='Orthogonal Slice (Y)', visible=False)
         self.top_figure.xaxis.axis_label = 'X'
         self.top_figure.yaxis.axis_label = 'E'
-        self.corner_figure = figure()
+        self.corner_figure = figure(title='IR Spectra')
         self.corner_figure.xaxis.axis_label = 'E'
         self.corner_figure.yaxis.axis_label = 'I'
-        self.pair_plot_figure = figure(tools="lasso_select")
-        self.decomposition_figure = figure()
+        self.pair_plot_figure = figure(title='Pair Plot', tools="lasso_select", visible=False)
+        self.decomposition_figure = figure(title='Decomposition Maps', visible=False)
         self.decomposition_figure.xaxis.axis_label = 'X'
         self.decomposition_figure.yaxis.axis_label = 'Y'
 
+        # Initialize data sources
+        self.spectra_source = ColumnDataSource(data={'E': [], 'I': []})
+        self.pair_plot_source = ColumnDataSource(data={'x': [], 'y': []})
+        self.main_image_source = ColumnDataSource(data={'image': []})
+        self.main_overlay_source = ColumnDataSource(data={'image': []})
+        self.right_image_source = ColumnDataSource(data={'image': []})
+        self.top_image_source = ColumnDataSource(data={'image': []})
+        self.decomposition_image_source = ColumnDataSource(data={'image': []})
+
         # Initialize glyphs
+        self.main_figure.image(image='image', source=self.main_image_source, x=self.bounds[2][0], y=self.bounds[1][0], dw=self.bounds[2][1], dh=self.bounds[1][1])
+        self.main_figure.image(image='image', source=self.main_overlay_source, x=self.bounds[2][0], y=self.bounds[1][0], dw=self.bounds[2][1], dh=self.bounds[2][1], global_alpha=.3, palette="OrRd3")
+        self.right_figure.image(image='image', source=self.right_image_source, x=self.bounds[0][0], y=self.bounds[1][0], dw=self.bounds[0][1], dh=self.bounds[1][1])
+        self.top_figure.image(image='image', source=self.top_image_source, x=self.bounds[2][0], y=self.bounds[0][0], dw=self.bounds[2][1], dh=self.bounds[0][1])
+        self.decomposition_figure.image(image='image', source=self.decomposition_image_source, x=self.bounds[2][0], y=self.bounds[1][0], dw=self.bounds[2][1], dh=self.bounds[1][1])
         self.main_x_line = Span(location=0, dimension='height', line_color='cyan', line_width=3)
         self.main_y_line = Span(location=0, dimension='width', line_color='cyan', line_width=3)
         self.right_E_line = Span(location=0, dimension='height', line_color='cyan', line_width=3)
@@ -54,11 +72,7 @@ class Viewer:
         self.top_figure.renderers.extend([self.top_x_line, self.top_E_line])
         self.corner_figure.renderers.append(self.corner_E_line)
         self.decomposition_figure.renderers.extend([self.decomposition_x_line, self.decomposition_y_line])
-
-        # Initialize data sources
-        self.spectra_source = ColumnDataSource(data={'E': [], 'I': []})
         self.corner_figure.line(x='E', y='I', source=self.spectra_source)
-        self.pair_plot_source = ColumnDataSource(data={'x': [], 'y': []})
         self.pair_plot_figure.circle(x='x', y='y', source=self.pair_plot_source)#, fill_alpha=0.2, size=10)
 
         # Build proxy toolbar
@@ -69,20 +83,20 @@ class Viewer:
                    self.pair_plot_figure,
                    self.decomposition_figure]
         for f in figures:
-            figure.toolbar_location = None
+            f.toolbar_location = None
         proxy_toolbar = ProxyToolbar(toolbars=list(map(lambda f: f.toolbar, figures)),
-                                     tools=sum([ f.toolbar.tools for f in figures ], []))
+                                     tools=sum([f.toolbar.tools for f in figures], []))
         toolbar = ToolbarBox(toolbar=proxy_toolbar, toolbar_location='above')
 
         # Build widgets
-        component_labels = [f'Component #{i+1}' for i in range(decompostion.shape[0])]
+        component_labels = [f'Component #{i+1}' for i in range(decomposition.shape[0])]
         self.decomposition_component = RadioButtonGroup(labels=component_labels, active=0)
         self.pair_plot_component_1 = RadioButtonGroup(labels=component_labels, active=0)
         self.pair_plot_component_2 = RadioButtonGroup(labels=component_labels, active=1)
         self.toggle_spectra_button = Toggle(label='Show Spectra', active=True)
-        self.toggle_pair_plot_button = Toggle(label='Show Pair Plot', active=decompostion is not None)
-        self.toggle_decomposition_button = Toggle(label='Show Decomposition', active=decompostion is not None)
-        self.toggle_orthogonal_button = Toggle(label='Show Orthogonal Slices', active=True)
+        self.toggle_pair_plot_button = Toggle(label='Show Pair Plot', active=False) #active=decompostion is not None)
+        self.toggle_decomposition_button = Toggle(label='Show Decomposition', active=False) #, active=decompostion is not None)
+        self.toggle_orthogonal_button = Toggle(label='Show Orthogonal Slices', active=False)
 
         # Bind events
         self.main_figure.on_event(Tap, partial(self.click_event, axes=[2, 1]))
@@ -91,27 +105,28 @@ class Viewer:
         self.corner_figure.on_event(Tap, partial(self.click_event, axes=[0]))
         self.decomposition_figure.on_event(Tap, partial(self.click_event, axes=[2, 1]))
 
-        self.corner_figure.on_event(Tap, self.debug)
-
         self.decomposition_component.on_change('active', lambda attr, old, new: self.show_component())
         self.toggle_decomposition_button.on_change('active', lambda attr, old, new: setattr(self.decomposition_figure, 'visible', new))
         self.toggle_spectra_button.on_change('active', lambda attr, old, new: setattr(self.corner_figure, 'visible', new))
         self.toggle_pair_plot_button.on_change('active', lambda attr, old, new: setattr(self.pair_plot_figure, 'visible', new))
-        # self.toggle_orthogonal_button.on_change('active', lambda attr, old, new: setattr(self.pair_plot_figure, 'visible', new))
+        self.toggle_orthogonal_button.on_change('active', lambda attr, old, new: (setattr(self.right_figure, 'visible', new),
+                                                                                  setattr(self.top_figure, 'visible', new)))
 
         self.pair_plot_component_1.on_change('active', lambda attr, old, new: self.show_pair_plot())
         self.pair_plot_component_2.on_change('active', lambda attr, old, new: self.show_pair_plot())
         self.pair_plot_source.selected.on_change('indices', self.show_pair_plot_selection)
 
         # Build layout
-        self.div = Div()
-        self.info_column = column(self.div)
+        self.location_div = Div()
+        self.info_column = column(self.location_div)
         self.info_panel = Panel(child=self.info_column, title="Info")
-        self.settings_column = column(self.decomposition_component,
-                                      self.toggle_spectra_button,
+        self.settings_column = column(self.toggle_spectra_button,
                                       self.toggle_decomposition_button,
                                       self.toggle_pair_plot_button,
                                       self.toggle_orthogonal_button,
+                                      Div(text='Decomposition Component'),
+                                      self.decomposition_component,
+                                      Div(text='Pair Plot Components'),
                                       self.pair_plot_component_1,
                                       self.pair_plot_component_2)
         self.settings_panel = Panel(child=self.settings_column, title="Settings")
@@ -123,13 +138,18 @@ class Viewer:
         # Todo: implement merged toolbars
         self.widget = layout([[toolbar],
                               [self.main_figure, self.control_tabs],
+                              [self.top_figure, self.right_figure],
                               [self.corner_figure],
-                              [self.pair_plot_figure, self.decomposition_figure]])
+                              [self.decomposition_figure, self.pair_plot_figure]])
 
         # Initialize views
         self.show_location(self.location, [0, 1, 2])
         self.show_component()
         self.show_pair_plot()
+
+    @property
+    def bin_location(self):
+        return tuple(map(int, self.location))
 
     def click_event(self, event, axes):
         x, y = event.x, event.y
@@ -138,14 +158,13 @@ class Viewer:
     def show_location(self, location, axes):
         for val, axis in zip(location, axes):
             self.location[axis] = val
+            self.print_location()
 
-        bin_location = list(map(int, self.location))
-
-        self.main_figure.image(image=[np.asarray(self.data[bin_location[0]])], x=0, y=0, dw=self.data.shape[2], dh=self.data.shape[1])
-        self.right_figure.image(image=[np.asarray(self.data[:, :, bin_location[2]].T)], x=0, y=0, dw=self.data.shape[0], dh=self.data.shape[1])
-        self.top_figure.image(image=[np.asarray(self.data[:, bin_location[1]])], x=0, y=0, dw=self.data.shape[2], dh=self.data.shape[0])
-        spectra_data = np.asarray(self.data[:, int(bin_location[1]), int(bin_location[2])])
-        self.spectra_source.data.update({'E': np.arange(len(spectra_data)),
+        self.main_image_source.data.update({'image': [np.asarray(self.data[self.bin_location[0]])]})
+        self.right_image_source.data.update({'image': [np.asarray(self.data[:, :, self.bin_location[2]].T)]})
+        self.top_image_source.data.update({'image': [np.asarray(self.data[:, self.bin_location[1]])]})
+        spectra_data = np.asarray(self.data[:, int(self.bin_location[1]), int(self.bin_location[2])])
+        self.spectra_source.data.update({'E': np.linspace(self.bounds[0][0], self.bounds[0][0]+self.bounds[0][1], len(spectra_data)),
                                          'I': spectra_data})
 
         self.main_x_line.location = self.location[2]
@@ -159,7 +178,7 @@ class Viewer:
         self.decomposition_y_line.location = self.location[1]
 
     def show_component(self):
-        self.decomposition_figure.image(image=[np.asarray(self.decomposition[self.decomposition_component.active])], x=0, y=0, dw=self.data.shape[2], dh=self.data.shape[1])
+        self.decomposition_image_source.data.update({'image':[np.asarray(self.decomposition[self.decomposition_component.active])]})
 
     def show_pair_plot(self):
         self.pair_plot_source.data.update({'x': np.asarray(self.decomposition[self.pair_plot_component_1.active].ravel()),
@@ -169,10 +188,15 @@ class Viewer:
 
     def show_pair_plot_selection(self, attr, old, new):
         if old != new:
-            self.div.text = str(np.unravel_index(new, self.data[0].shape))
+            mask = np.ones(self.data[0].shape)
+            mask.ravel()[new] = 0
+            self.main_overlay_source.data.update({'image': [mask]})
 
-    def debug(self, event):
-        self.div.text = f'x: {event.x:.2f}, y: {event.y:.2f}'
+    def print_location(self):
+        self.location_div.text = f'x: {self.location[2]:.2f}, ' \
+                                 f'y: {self.location[1]:.2f}, ' \
+                                 f'E: {self.location[0]:.2f}, ' \
+                                 f'I: {self.data[self.bin_location[0], self.bin_location[1], self.bin_location[2]].compute():.2f}'
 
 
 def notebook_viewer(data, location=None, theme='dark_minimal'):
