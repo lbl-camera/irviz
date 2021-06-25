use patterned graph ids; refactor graph classes; fix dx/dy off by one

irviz/graphs/decomposition.py
@@ -14,7 +14,7 @@ __all__ = ['DecompositionGraph']
 class DecompositionGraph(SliceGraph):
     title = 'Decomposition Maps'
 
-    def __init__(self, data, bounds, parent, *args, **kwargs):
+    def __init__(self, data, bounds, cluster_labels, cluster_label_names, parent, *args, **kwargs):
 
         traces = []
         for i in range(data.shape[0]):
@@ -24,20 +24,27 @@ class DecompositionGraph(SliceGraph):
             traces.append(go.Heatmap(z=np.asarray(data[i]),
                                  colorscale=color_scale,
                                  y0=bounds[1][0],
-                                 dy=(bounds[1][1]-bounds[1][0])/data.shape[1],
+                                 dy=(bounds[1][1]-bounds[1][0])/(data.shape[1]-1),
                                  x0=bounds[2][0],
-                                 dx=(bounds[2][1]-bounds[2][0])/data.shape[2],
+                                 dx=(bounds[2][1]-bounds[2][0])/(data.shape[2]-1),
                                  visible=(i==0),
                                  opacity=.5 if i else 1,
                                  ))
 
         kwargs['traces'] = traces
 
-        super(DecompositionGraph, self).__init__(data, bounds, parent, *args, **kwargs)
+        super(DecompositionGraph, self).__init__(data, bounds, cluster_labels, cluster_label_names, parent, *args, **kwargs)
+
+        # Hide the free image trace
+        self._image.visible = False
+
+    def _id(self):
+        _id = super(DecompositionGraph, self)._id()
+        _id['subtype'] = 'decomposition'
+        return _id
 
     def register_callbacks(self):
-        # Set up callbacks
-        # ----------------
+        super(DecompositionGraph, self).register_callbacks()
 
         # Wire-up visibility toggle
         self._parent._app.callback(
@@ -49,12 +56,6 @@ class DecompositionGraph(SliceGraph):
         targeted_callback(self.set_component_opacity,
                           Input({'type': 'component-opacity', 'index': ALL}, 'value'),
                           Output(self.id, 'figure'),
-                          app=self._parent._app)
-
-        # Disable sliders when their component is hidden
-        targeted_callback(self.disable_sliders,
-                          Input(self._parent.decomposition_component_selector.id, 'value'),
-                          Output(self._parent.component_opacity_sliders.id, 'children'),
                           app=self._parent._app)
 
         # Show components when selected
@@ -134,11 +135,7 @@ class DecompositionGraph(SliceGraph):
 
         return self._update_figure()
 
-    def disable_sliders(self, component_indices):
-        for i, trace in enumerate(self._traces):
-            self._opacity_slider(i).disabled = not (i in component_indices)  # TODO: set this in a separate callback that outputs to the slider
 
-        return self._parent.component_opacity_sliders.children
 
 
     @staticmethod

irviz/graphs/pair_plot.py
@@ -109,11 +109,11 @@ class PairPlotGraph(dcc.Graph):
         # Default None - Any non-array value passed to selectedpoints kwarg indicates there is no selection present
         selected_points = None
         triggered = dash.callback_context.triggered
-        if self._parent.map_graph.id in triggered[0]['prop_id']:
+        if self._parent.map_graph.id == triggered[0]['prop_id']:
             # selected data being None indicates that the user has selected data
             # selected data 'points' being empty indicates the user has selected data outside of the region
             selected_points = self._indexes_from_selection(map_selectedData)
-        elif self._parent.optical_graph.id in triggered[0]['prop_id']:
+        elif self._parent.optical_graph.id == triggered[0]['prop_id']:
             selected_points = self._indexes_from_selection(optical_selectedData)
 
         self.traces.append(go.Scattergl(x=np.asarray(x.ravel()),

irviz/graphs/slice.py
@@ -1,7 +1,12 @@
 from itertools import count
 
+import numpy as np
 import dash_core_components as dcc
+from dash.dependencies import Input, Output, ALL
 from plotly import graph_objects as go
+
+from irviz.utils.dash import targeted_callback
+from irviz.utils.math import nearest_bin
 
 
 class SliceGraph(dcc.Graph):
@@ -15,24 +20,22 @@ class SliceGraph(dcc.Graph):
         The parent object that creates this Graph
 
     """
-    _counter = count(0)
 
     title = ''
     aspect_locked = True
 
-    def __init__(self, data, bounds, parent, slice_axis=0, traces=None, shapes=None, **kwargs):
+    def __init__(self, data, bounds, cluster_labels, cluster_label_names, parent, slice_axis=0, traces=None, shapes=None, **kwargs):
 
         # Cache our data and parent for use in the callbacks
         self._data = data
         self._parent = parent
         self._bounds = bounds
-        self._instance_index = next(self._counter)
         self._traces = traces or []
         self._shapes = shapes or []
-
         self.xaxis_title = kwargs.pop('xaxis_title', '')
         self.yaxis_title = kwargs.pop('yaxis_title', '')
 
+        # Build shapes
         self._h_line = go.layout.Shape(type='line',
                                        # width=3,
                                        xref='paper',
@@ -49,7 +52,66 @@ class SliceGraph(dcc.Graph):
                                        y0=0,
                                        y1=1)
 
+        # Build traces
+        default_slice_index = (data.shape[0] - 1) // 2
+        graph_bounds = dict(y0=bounds[1][0],
+                            dy=(bounds[1][1]-bounds[1][0])/(data.shape[1]-1),
+                            x0=bounds[2][0],
+                            dx=(bounds[2][1]-bounds[2][0])/(data.shape[2]-1))
+
+        if data.ndim == 4 and data.shape[-1] == 3:
+            graph_object = go.Image
+        else:
+            graph_object = go.Heatmap
+
+        self._image = graph_object(z=np.asarray(data[default_slice_index]),
+                                   colorscale='viridis',
+                                   # hovertemplate=hovertemplate,
+                                   **graph_bounds,
+                                   # **extra_kwargs
+                                   )
+
+        extra_kwargs = {}
+        x_label = kwargs.get('xaxis_title', '')
+        y_label = kwargs.get('yaxis_title', '')
+        if cluster_label_names is not None and cluster_labels is not None:
+            extra_kwargs['text'] = np.asarray(cluster_label_names)[cluster_labels]
+            hovertemplate = f'{x_label}: %{{x}}<br />{y_label}: %{{y}}<br />I: %{{z}}<br />Label: %{{text}}<extra></extra>'
+        else:
+            hovertemplate = f'{x_label}: %{{x}}<br />{y_label}: %{{y}}<br />I: %{{z}}<extra></extra>'
+
+        self._selection_mask = go.Heatmap(z=np.ones(data[0].shape) * np.NaN,
+                                          colorscale='reds',
+                                          opacity=0.3,
+                                          showscale=False,
+                                          hoverinfo='skip',
+                                          **graph_bounds
+                                          )
+        x, y = np.meshgrid(np.linspace(bounds[2][0], bounds[2][1], data.shape[2]),
+                           np.linspace(bounds[1][0], bounds[1][1], data.shape[1]))
+
+        # This dummy scatter trace is added to support lasso selection
+        self._dummy_scatter = go.Scattergl(x=x.ravel(),
+                                           y=y.ravel(),
+                                           mode='markers',
+                                           marker={'color': 'rgba(0,0,0,0)'},
+                                           hoverinfo='skip'
+                                           )
+        # Add another transparent heatmap overlay for labels
+        self._clusters = go.Heatmap(z=np.ones(data[0].shape) * np.NaN,
+                                    colorscale='Portland',
+                                    **graph_bounds,
+                                    opacity=0.3,
+                                    showscale=False,
+                                    hovertemplate=hovertemplate,
+                                    hoverinfo='skip',
+                                    **extra_kwargs
+                                    )
+        if cluster_labels is not None:
+            self._clusters.z = cluster_labels  # NaNs are transparent
+
         self._shapes.extend([self._h_line, self._v_line])
+        self._traces = (traces or []) + [self._dummy_scatter, self._image, self._selection_mask, self._clusters]
 
         figure = self._update_figure()
         super(SliceGraph, self).__init__(figure=figure,
@@ -63,10 +125,72 @@ class SliceGraph(dcc.Graph):
                                          **kwargs)
 
     def _id(self):
-        return f'slicegraph_{self._instance_index}'
+        return {'type': 'slice_graph',
+                'subtype': ...,
+                'index': self._parent._instance_index}
 
     def register_callbacks(self):
-        ...
+        # When any SliceGraph is clicked, update its x,y slicer lines
+        targeted_callback(self.show_click,
+                          Input({'type': 'slice_graph',
+                                 'subtype': ALL,
+                                 'index': self._parent._instance_index},
+                                'clickData'),
+                          Output(self.id, 'figure'),
+                          app=self._parent._app)
+
+        # When the optical graph is clicked show the same position
+        targeted_callback(self.show_click,
+                          Input(self._parent.optical_graph.id, 'clickData'),
+                          Output(self.id, 'figure'),
+                          app=self._parent._app)
+
+        # When the decomposition graph is clicked show the same position
+        targeted_callback(self.show_click,
+                          Input(self._parent.decomposition_graph.id, 'clickData'),
+                          Output(self.id, 'figure'),
+                          app=self._parent._app)
+
+        # Change color scale from selector
+        targeted_callback(self.set_color_scale,
+                          Input(self._parent.map_color_scale_selector.id, 'label'),
+                          Output(self.id, 'figure'),
+                          app=self._parent._app)
+
+        # When points are selected in the pair plot, show them here
+        targeted_callback(self._show_selection_mask,
+                          Input(self._parent.pair_plot_graph.id, 'selectedData'),
+                          Output(self.id, 'figure'),
+                          app=self._parent._app)
+
+        # When this SliceGraph is lasso'd, update the selection mask
+        targeted_callback(self._show_selection_mask,
+                          Input(self.id, 'selectedData'),
+                          Output(self.id, 'figure'),
+                          app=self._parent._app)
+
+        # Bind the labels toggle to its trace's visibility
+        targeted_callback(self.set_clusters_visibility,
+                          Input(self._parent.graph_toggles.id, 'value'),
+                          Output(self.id, 'figure'),
+                          app=self._parent._app)
+
+    def set_color_scale(self, color_scale):
+        self._image.colorscale = color_scale
+
+        return self._update_figure()
+
+    def set_clusters_visibility(self, value):
+        self._clusters.visible = 'show_clusters' in value
+
+        return self._update_figure()
+
+    def update_slice(self, spectra_graph_click_data):
+        slice = spectra_graph_click_data["points"][0]["x"]
+        slice_index = nearest_bin(slice, self._bounds[0], self._data.shape[0])
+        self._image.z = np.asarray(self._data[slice_index])
+
+        return self._update_figure()
 
     def _update_figure(self):
         fig = go.Figure(self._traces)
@@ -86,6 +210,26 @@ class SliceGraph(dcc.Graph):
         self._h_line.y1 = y_index
         self._v_line.x0 = x_index
         self._v_line.x1 = x_index
+
+        return self._update_figure()
+
+    def _show_selection_mask(self, selection):
+        # Check two cases:
+        #     1. selection is None: initial state (no selection) or user has dbl-clicked w/ lasso/selection tool
+        #     2. selection['points'] is empty: user has selected no points
+        if selection is not None and len(selection['points']) > 0:
+            # Get x,y from the raveled indexes
+            raveled_indexes = list(map(lambda point: point['pointIndex'],
+                                       filter(lambda point: point['curveNumber'] == 0,
+                                              selection['points'])))
+            mask = np.zeros(self._data[0].shape)
+            # Cannot be 0s - must be NaNs (eval to None) so it doesn't affect underlying HeatMap
+            mask.fill(np.NaN)
+            mask.ravel()[raveled_indexes] = 1
+            # Create overlay
+            self._selection_mask.z = mask
+        else:
+            self._selection_mask.z = np.ones(self._data[0].shape) * np.NaN
 
         return self._update_figure()
 

irviz/viewer.py
@@ -1,5 +1,6 @@
 import numbers
 import warnings
+from itertools import count
 
 import dash_bootstrap_components as dbc
 import dash_html_components as html
@@ -20,7 +21,7 @@ from irviz.utils.dash import targeted_callback
 class Viewer(html.Div):
     """Interactive viewer that creates and contains all of the visualized components within the Dash app"""
 
-    _global_slicer_counter = 0
+    _instance_counter = count(0)
 
     def __init__(self,
                  app,
@@ -90,9 +91,9 @@ class Viewer(html.Div):
                 }
         """
 
-        Viewer._global_slicer_counter += 1
         self.data = data
         self._app = app
+        self._instance_index = next(self._instance_counter)
         self.decomposition = decomposition
 
         self.bounds = np.asarray(bounds)
@@ -155,6 +156,8 @@ class Viewer(html.Div):
         if self.decomposition is not None:
             self.decomposition_graph = DecompositionGraph(self.decomposition,
                                                           self.bounds,
+                                                          cluster_labels,
+                                                          cluster_label_names,
                                                           self,
                                                           xaxis_title=x_axis_title,
                                                           yaxis_title=y_axis_title)
@@ -206,7 +209,7 @@ class Viewer(html.Div):
 
             self.decomposition_component_selector = dbc.Checklist(id='decomposition-component-selector',
                                                                   value=[0],
-                                                                  style={'paddingLeft':0, 'paddingRight':0},
+                                                                  style={'paddingLeft': 0, 'paddingRight': 0},
                                                                   **radio_kwargs)
 
             self.component_opacity_sliders = html.Div(
@@ -221,7 +224,7 @@ class Viewer(html.Div):
                     disabled=True if i else False
                 ) for i in range(self.decomposition.shape[0])],
                 className='col-sm',
-                style={'paddingLeft':0, 'paddingRight':0},
+                style={'paddingLeft': 0, 'paddingRight': 0},
                 id='component-opacity-sliders'
             )
 
@@ -236,6 +239,12 @@ class Viewer(html.Div):
                 className='col-sm-auto',
                 style={'paddingLeft':0, 'paddingRight':0, 'marginTop':2.5},
             )
+
+            # Disable sliders when their component is hidden
+            targeted_callback(self.disable_sliders,
+                              Input(self.decomposition_component_selector.id, 'value'),
+                              Output(self.component_opacity_sliders.id, 'children'),
+                              app=self._app)
 
             decomposition_selector_layout = html.Div(
                 [
@@ -345,6 +354,12 @@ class Viewer(html.Div):
                                      className='container-fluid',
                                      )
 
+    def disable_sliders(self, component_indices):
+        for i, trace in enumerate(self._traces):
+            self._opacity_slider(i).disabled = not (i in component_indices)  # TODO: set this in a separate callback that outputs to the slider
+
+        return self._parent.component_opacity_sliders.children
+
     @property
     def spectrum(self):
         """The currently shown spectrum energy/wavenumber and intensity values"""
@@ -369,6 +384,7 @@ class Viewer(html.Div):
     def position(self):
         """The spatial position of the current spectrum"""
         return self.spectra_graph.position
+
 
 
 def notebook_viewer(data,