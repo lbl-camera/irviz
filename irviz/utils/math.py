a187ab4e00009c9297d3f4b5af9dbb4111c74360
ronpandolfi <ronpandolfi@gmail.com>
2021-07-01 14:13:14 -0700

    Fix off-by-one in nearest_bin math

--- irviz/utils/math.py
+++ irviz/utils/math.py
@@ -1,4 +1,4 @@
 def nearest_bin(x, bounds, bin_count, rounding=True):
-    if round:
-        return round((x-bounds[0])/(bounds[1]-bounds[0])*bin_count)
-    return int((x-bounds[0])/(bounds[1]-bounds[0])*bin_count)
+    if rounding:
+        return round((x-bounds[0])/(bounds[1]-bounds[0])*(bin_count-1))
+    return int((x-bounds[0])/(bounds[1]-bounds[0])*(bin_count-1))
