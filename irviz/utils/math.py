3792ecd5c0693b662a7413bcb037f433cf4eb65f
ronpandolfi <ronpandolfi@gmail.com>
2021-08-24 14:46:34 -0700

    support per-bin bounds

--- irviz/utils/math.py
+++ irviz/utils/math.py
@@ -1,4 +1,10 @@
+import numpy as np
+
+
 def nearest_bin(x, bounds, bin_count, rounding=True):
-    if rounding:
-        return round((x - bounds[0]) / (bounds[1] - bounds[0]) * (bin_count - 1))
-    return int((x - bounds[0]) / (bounds[1] - bounds[0]) * (bin_count - 1))
+    if len(bounds) == 2:
+        if rounding:
+            return round((x - bounds[0]) / (bounds[1] - bounds[0]) * (bin_count - 1))
+        return int((x - bounds[0]) / (bounds[1] - bounds[0]) * (bin_count - 1))
+    else:  # for per-bin valued bounds
+        return np.abs(np.array(bounds) - x).argmin()
