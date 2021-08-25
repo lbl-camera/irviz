c318214868f545bd061655bc4c18ccba2674bd77
ronpandolfi <ronpandolfi@gmail.com>
2021-08-25 11:43:32 -0700

    Add contribution recommendations; add filter test module

--- irviz/background_filters/__init__.py
+++ irviz/background_filters/__init__.py
@@ -1,2 +1,12 @@
 from .emsc import *
 from .gpr import *
+
+# Standards for contribution
+# - Filters should avoid raising errors; errors will be printed to the user on every execution
+# - Filters should be added to testing (see irviz.tests.test_filters)
+# - Code should be well formatted, either with Pycharm's formatter (Ctrl+Alt+L?) or Black; line-lengths < 120 ideally
+# - Code should be cleaned with linter (see yellow squiggle underlines in pycharm)
+# - Doc strings required for public API components
+# - Contextual comments throughout code, particularly for high-complexity lines
+# - Avoid unnecessary use of external packages where possible to limit dependency bloat
+# - Utilize dask laziness when possible/appropriate to minimize memory footprint in support of large datasets
