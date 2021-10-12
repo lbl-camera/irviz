e2d1fcaa5e52b6251915fae00e4c810ba1d5880a
ronpandolfi <ronpandolfi@gmail.com>
2021-10-12 12:22:25 -0700

    Allow data update in decomposition graph; make layer controls dynamic

--- irviz/graphs/decomposition.py
+++ irviz/graphs/decomposition.py
@@ -2,7 +2,9 @@ import re
 
 import dash
 import numpy as np
-from dash.dependencies import Output, Input, ALL
+from dash._utils import stringify_id
+from dash.dependencies import Output, Input, ALL, MATCH, State
+from dash.exceptions import PreventUpdate
 from plotly import graph_objects as go
 import dash_bootstrap_components as dbc
 from dash import html
@@ -19,6 +21,11 @@ __all__ = ['DecompositionGraph']
 
 class DecompositionGraphPanel(Panel):
     def __init__(self, instance_index, component_count, cluster_labels):
+        self._checkboxes = []
+        self._check_labels = []
+        self._component_color_scale_selectors = []
+        self._component_opacity_sliders = []
+
         self._cluster_overlay_opacity = dcc.Slider(id={'type': 'cluster-opacity',
                                                        'index': instance_index,
                                                        'subtype': 'decomposition'},
@@ -29,63 +36,9 @@ class DecompositionGraphPanel(Panel):
                                                    className='centered-slider',
                                                    disabled=True if cluster_labels is None else False,
                                                    )
+        items = [self.build_item(i) for i in range(component_count)]
 
-        radio_kwargs = dict(className='btn-group-vertical col-sm-auto',
-                            labelClassName="btn btn-secondary",
-                            labelCheckedClassName="active",
-                            options=[{'label': f'{i + 1}', 'value': i}
-                                     for i in range(component_count)]
-
-                            )
-
-        self._decomposition_component_selector = dbc.Checklist(id='decomposition-component-selector',
-                                                               value=[0],
-                                                               style={'paddingLeft': 0, 'paddingRight': 0},
-                                                               **radio_kwargs)
-
-        self._component_opacity_sliders = html.Div(
-            [dcc.Slider(
-                id={'type': 'component-opacity',
-                    'index': i},
-                min=0,
-                max=1,
-                step=.1,
-                value=.5 if i else 1,
-                className='centered-slider',
-                disabled=True if i else False
-            ) for i in range(component_count)],
-            className='col-sm',
-            style={'paddingLeft': 0, 'paddingRight': 0},
-            id='component-opacity-sliders'
-        )
-
-        self.color_scale_selectors = [ColorScaleSelector({'type': 'color-scale-selector',
-                                                          'index': i},
-                                                         values=decomposition_color_scales,
-                                                         value=decomposition_color_scales[
-                                                             i % len(decomposition_color_scales)],
-                                                         )
-                                      for i in range(component_count)]
-
-        self._component_color_scale_selectors = html.Div(self.color_scale_selectors,
-                                                         className='col-sm-auto',
-                                                         style={'paddingLeft': 0, 'paddingRight': 0, 'marginTop': 2.5},
-                                                         )
-
-        decomposition_selector_layout = dbc.FormGroup(
-            [
-                html.Div([
-                    html.Div([self._decomposition_component_selector,
-                              self._component_color_scale_selectors,
-                              self._component_opacity_sliders, ],
-                             className='row well'
-                             ),
-                ],
-                    className='container'
-                ),
-            ],
-            className='radio-group'
-        )
+        self.decomposition_selector_layout = dbc.FormGroup(items, id='decomposition-selector-layout')
 
         self.visibility_toggle = dbc.Checkbox(id=dict(type='decomposition-visibility',
                                                       instance_index=instance_index),
@@ -94,7 +47,7 @@ class DecompositionGraphPanel(Panel):
         children = [dbc.FormGroup([self.visibility_toggle,
                                    dbc.Label('Show Decomposition Image')]),
                     dbc.FormGroup([dbc.Label('Component Color Themes'),
-                                   decomposition_selector_layout]),
+                                   self.decomposition_selector_layout]),
                     dbc.FormGroup(
                         [dbc.Label("Cluster Label Overlay Opacity"), self._cluster_overlay_opacity])]
 
@@ -102,20 +55,88 @@ class DecompositionGraphPanel(Panel):
 
     def init_callbacks(self, app):
         super(DecompositionGraphPanel, self).init_callbacks(app)
-        for color_scale_selector in self.color_scale_selectors:
+        for color_scale_selector in self._component_color_scale_selectors:
             color_scale_selector.init_callbacks(app)
 
         # Disable sliders when their component is hidden
-        targeted_callback(self.disable_sliders,
-                          Input(self._decomposition_component_selector.id, 'value'),
-                          Output(self._component_opacity_sliders.id, 'children'),
+        targeted_callback(self.disable_slider,
+                          Input(dict(type='decomposition-component-toggle', index=MATCH), 'checked'),
+                          Output(dict(type='component-opacity', index=MATCH), 'disabled'),
                           app=app)
 
-    def disable_sliders(self, component_indices):
-        for i, slider in enumerate(self._component_opacity_sliders.children):
-            slider.disabled = not (i in component_indices)
+        # Change number of items when decomposition changes
+        targeted_callback(self.update_items,
+                          Input(dict(type='decomposition-status', pattern=ALL), 'data'),
+                          Output(self.decomposition_selector_layout.id, 'children'),
+                          app=app)
 
-        return self._component_opacity_sliders.children
+        targeted_callback(self.update_check_state,
+                          Input(dict(type='decomposition-component-toggle', index=MATCH), 'checked'),
+                          Output(dict(type='decomposition-component-label', index=MATCH), 'className'),
+                          State(dict(type='decomposition-component-label', index=MATCH), 'className'),
+                          app=app)
+
+    def build_item(self, i):
+        checkbox = dbc.Checkbox(id=dict(type='decomposition-component-toggle', index=i),
+                                 checked=(i==0),
+                                 style=dict(display='none'),
+                                 # className='btn-group-vertical col-sm-auto',
+                                 # labelClassName="btn btn-secondary",
+                                 # labelCheckedClassName="active",
+                                )
+        check_label = dbc.Label(id=dict(type='decomposition-component-label', index=i),
+                                children=f'{i+1}',
+                                className='btn btn-secondary' + (' active' if checkbox.checked else ''),
+                                html_for=stringify_id(checkbox.id))
+        color_scale_selector = ColorScaleSelector({'type': 'decomposition-color-scale-selector',
+                            'index': i},
+                           values=decomposition_color_scales,
+                           value=decomposition_color_scales[
+                               i % len(decomposition_color_scales)],
+                           )
+        slider = dcc.Slider(
+            id={'type': 'component-opacity',
+                'index': i},
+            min=0,
+            max=1,
+            step=.1,
+            value=.5 if i else 1,
+            className='centered-slider col',
+            disabled=True if i else False
+        )
+        self._checkboxes.append(checkbox)
+        self._check_labels.append(check_label)
+        self._component_color_scale_selectors.append(color_scale_selector)
+        self._component_opacity_sliders.append(slider)
+
+        return html.Div([checkbox, check_label, color_scale_selector, slider], className='row well', style=dict())
+
+    def disable_slider(self, checked):
+        return not checked
+
+    def update_items(self, n_items):
+        items = self.decomposition_selector_layout.children
+
+        # set display style for each existing item
+        for i, item in enumerate(items):
+            item.style['display'] = 'none' if i > n_items else 'flex'
+
+        # if more items are required, build them
+        if n_items > len(items):
+            for i in range(len(items), n_items):
+                items.append(self.build_item(i))
+
+        return items
+
+    def update_check_state(self, checked):
+        className = set(next(iter(dash.callback_context.states.values())).split(' '))
+        if checked:
+            className.add('active')
+        else:
+            className.remove('active')
+        return ' '.join(list(className))
+
+
 
 
 class DecompositionGraph(SliceGraph):
@@ -123,21 +144,9 @@ class DecompositionGraph(SliceGraph):
 
     def __init__(self, data, instance_index, cluster_labels, cluster_label_names, bounds, *args, **kwargs):
         self.configuration_panel = DecompositionGraphPanel(instance_index, data.shape[0], cluster_labels)
+        self.bounds = bounds
 
-        self._component_traces = []
-        for i in range(data.shape[0]):
-            color_scale = decomposition_color_scales[i % len(decomposition_color_scales)]
-            color_scale = transparent_color_scales.get(color_scale, color_scale)
-
-            self._component_traces.append(go.Heatmap(z=np.asarray(data[i]),
-                                                     colorscale=color_scale,
-                                                     y0=bounds[1][0],
-                                                     dy=(bounds[1][1] - bounds[1][0]) / (data.shape[1] - 1),
-                                                     x0=bounds[2][0],
-                                                     dx=(bounds[2][1] - bounds[2][0]) / (data.shape[2] - 1),
-                                                     visible=(i == 0),
-                                                     opacity=.5 if i else 1,
-                                                     ))
+        self._component_traces = self._build_component_heatmaps(data)
 
         kwargs['traces'] = self._component_traces
 
@@ -152,6 +161,32 @@ class DecompositionGraph(SliceGraph):
         # Hide the free image trace
         self._image.visible = False
         self.figure = self._update_figure()
+
+    def _build_component_heatmaps(self, data):
+        traces = []
+        for i in range(data.shape[0]):
+            color_scale = decomposition_color_scales[i % len(decomposition_color_scales)]
+            color_scale = transparent_color_scales.get(color_scale, color_scale)
+
+            trace = go.Heatmap(z=np.asarray(data[i]),
+                               colorscale=color_scale,
+                               y0=self.bounds[1][0],
+                               dy=(self.bounds[1][1] - self.bounds[1][0]) / (data.shape[1] - 1),
+                               x0=self.bounds[2][0],
+                               dx=(self.bounds[2][1] - self.bounds[2][0]) / (data.shape[2] - 1),
+                               visible=(i == 0),
+                               opacity=.5 if i else 1,
+                               )
+
+            traces.append(trace)
+        return traces
+
+    def rebuild_component_heatmaps(self, data):
+        for trace in self._component_traces:
+            self._traces.remove(trace)
+        self._component_traces = self._build_component_heatmaps(data)
+        self._traces = self._component_traces + self._traces
+        return self._update_figure()
 
     def _id(self, instance_index):
         _id = super(DecompositionGraph, self)._id(instance_index)
@@ -174,14 +209,14 @@ class DecompositionGraph(SliceGraph):
                           app=app)
 
         # Show components when selected
-        targeted_callback(self.show_components,
-                          Input(self.configuration_panel._decomposition_component_selector.id, 'value'),
+        targeted_callback(self.show_component,
+                          Input(dict(type='decomposition-component-toggle', index=ALL), 'checked'),
                           Output(self.id, 'figure'),
                           app=app)
 
         # Update the color scale when new item is selected
         targeted_callback(self.set_color_scale,
-                          Input({'type': 'color-scale-selector', 'index': ALL}, 'label'),
+                          Input({'type': 'decomposition-color-scale-selector', 'index': ALL}, 'label'),
                           Output(self.id, 'figure'),
                           app=app)
 
@@ -193,10 +228,10 @@ class DecompositionGraph(SliceGraph):
         return self._update_figure()
 
     def _opacity_slider(self, i):
-        return self.configuration_panel._component_opacity_sliders.children[i]
+        return self.configuration_panel._component_opacity_sliders[i]
 
     def _color_scale_selector(self, i):
-        return self.configuration_panel._component_color_scale_selectors.children[i]
+        return self.configuration_panel._component_color_scale_selectors[i]
 
     def set_component_opacity(self, value):
         i = int(re.findall('(?<="index":)\\d+(?=,)', dash.callback_context.triggered[0]['prop_id'])[0])
@@ -207,7 +242,7 @@ class DecompositionGraph(SliceGraph):
     def _update_opacity(self):
         # Get a sum of all enabled slider values minus the first enabled value
         total = 0
-        for slider in self.configuration_panel._component_opacity_sliders.children:
+        for slider in self.configuration_panel._component_opacity_sliders:
             if not slider.disabled:
                 total += slider.value
 
@@ -222,10 +257,14 @@ class DecompositionGraph(SliceGraph):
 
                 trace.opacity = self._opacity_slider(i).value / total
 
-    def show_components(self, component_indices):
-        for i, trace in enumerate(self._component_traces):
-            trace.visible = (i in component_indices)
-            trace.showscale = len(component_indices) < 2
+    def show_component(self, checked):
+        i = int(re.findall('(?<="index":)\\d+(?=,)', dash.callback_context.triggered[0]['prop_id'])[0])
+        if i > len(self._component_traces)-1:
+            raise PreventUpdate
+
+        trace = self._component_traces[i]
+        trace.visible = checked
+        trace.showscale = len(list(filter(lambda trace: trace.visible, self._component_traces))) < 2
         self._update_opacity()
 
         return self._update_figure()
