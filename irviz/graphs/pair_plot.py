5dea2d1bdc3fdd2c869eeed889374b5e91942c55
ronpandolfi <ronpandolfi@gmail.com>
2021-11-04 13:25:32 -0700

    Pair-plot refactor to allow live updates

--- irviz/graphs/pair_plot.py
+++ irviz/graphs/pair_plot.py
@@ -1,11 +1,13 @@
 import dash
 import numpy as np
-from dash.dependencies import Output, Input, ALL
+from dash._utils import stringify_id
+from dash.dependencies import Output, Input, ALL, State
 from dash.exceptions import PreventUpdate
 from plotly import graph_objects as go
 import dash_bootstrap_components as dbc
 from dash import dcc
 from dash import html
+import phonetic_alphabet
 
 from ryujin.components import Panel
 from ryujin.utils.dash import targeted_callback
@@ -52,6 +54,26 @@ class PairPlotGraphPanel(Panel):
     def init_callbacks(self, app):
         super(PairPlotGraphPanel, self).init_callbacks(app)
 
+        # Change number of items when decomposition changes
+        targeted_callback(self.update_items_1,
+                          Input(dict(type='decomposition-status', pattern=ALL), 'data'),
+                          Output(self._decomposition_component_1.id, 'options'),
+                          app=app)
+        targeted_callback(self.update_items_2,
+                          Input(dict(type='decomposition-status', pattern=ALL), 'data'),
+                          Output(self._decomposition_component_2.id, 'options'),
+                          app=app)
+
+    def update_items_1(self, component_count):
+        options = [{'label': f'{i + 1}', 'value': i}
+                   for i in range(component_count)]
+        return options
+
+    def update_items_2(self, component_count):
+        options = [{'label': f'{i + 1}', 'value': i}
+                   for i in range(component_count)] + [{'label': 'ALL', 'value': 'ALL'}]
+        return options
+
 
 class PairPlotGraph(dcc.Graph):
     title = 'Pair Plot'
@@ -69,9 +91,10 @@ class PairPlotGraph(dcc.Graph):
         self._cluster_labels = cluster_labels
         self._cluster_label_names = cluster_label_names
         self._bounds = bounds
-        self.traces = []
+        self._component_traces = []
         self.crosshair_trace = None
         self._crosshair_index = None
+        self.selected_points = None
 
         # Initialize persistent traces
         self.crosshair_trace = go.Scattergl(x=[],
@@ -83,7 +106,7 @@ class PairPlotGraph(dcc.Graph):
                                                     'line': {'width': 2}},
                                             hoverinfo='skip')
 
-        figure = self._update_figure()
+        figure = self.show_pair_plot(0, 1)
         super(PairPlotGraph, self).__init__(figure=figure,
                                             id=self._id(),
                                             **graph_kwargs or {})
@@ -100,21 +123,41 @@ class PairPlotGraph(dcc.Graph):
         # When the parent viewer's 'spectra_graph' is clicked
         #     we need to update the internal Figure for this Graph
         # When MapGraph is lasso'd, show that selection here too
-        # Note: this can't be a targeted callback, since multiple values are required
-        app.callback(
-            Output(self.id, 'figure'),
-            Input(self.configuration_panel._decomposition_component_1.id, 'value'),
-            Input(self.configuration_panel._decomposition_component_2.id, 'value'),
-            Input({'type': 'slice_graph',
-                   'subtype': ALL,
-                   'index': self._instance_index},
-                  'selectedData'),
-            # When any SliceGraph is clicked, update its x,y slicer lines
-            Input({'type': 'slice_graph',
-                   'subtype': ALL,
-                   'index': self._instance_index},
-                  'clickData'),
-        )(self.show_pair_plot)
+        # when component ids change update plotted data
+        targeted_callback(self.show_pair_plot,
+                          Input(self.configuration_panel._decomposition_component_1.id, 'value'),
+                          Output(self.id, 'figure'),
+                          State(self.configuration_panel._decomposition_component_1.id, 'value'),
+                          State(self.configuration_panel._decomposition_component_2.id, 'value'),
+                          app=app)
+        targeted_callback(self.show_pair_plot,
+                          Input(self.configuration_panel._decomposition_component_2.id, 'value'),
+                          Output(self.id, 'figure'),
+                          State(self.configuration_panel._decomposition_component_1.id, 'value'),
+                          State(self.configuration_panel._decomposition_component_2.id, 'value'),
+                          app=app)
+
+        # When any SliceGraph is clicked, update its highlighted point
+        targeted_callback(self.show_pair_plot,
+                          Input({'type': 'slice_graph',
+                                 'subtype': ALL,
+                                 'index': self._instance_index},
+                                'clickData'),
+                          Output(self.id, 'figure'),
+                          State(self.configuration_panel._decomposition_component_1.id, 'value'),
+                          State(self.configuration_panel._decomposition_component_2.id, 'value'),
+                          app=app)
+
+        # When any any SliceGraph is lasso'd, show that selection here too
+        targeted_callback(self.show_selection,
+                          Input({'type': 'slice_graph',
+                                 'subtype': ALL,
+                                 'index': self._instance_index},
+                                'selectedData'),
+                          Output(self.id, 'figure'),
+                          State(self.configuration_panel._decomposition_component_1.id, 'value'),
+                          State(self.configuration_panel._decomposition_component_2.id, 'value'),
+                          app=app)
 
         # Set up help notifications for selection tools
         targeted_callback(self._update_selection_help_text,
@@ -144,46 +187,43 @@ class PairPlotGraph(dcc.Graph):
 
     def _update_figure(self):
         """ Remake the figure to force a display update """
-        fig = go.Figure(self.traces)
+        fig = go.Figure(self._component_traces + [self.crosshair_trace])
         fig.update_layout(title=self.title,
                           xaxis_title=self._xaxis_title,
                           yaxis_title=self._yaxis_title)
         return fig
 
-    def show_pair_plot(self, component1, component2, selectedData, click_data):
-        if component1 is None or component2 is None:
-            raise PreventUpdate
+    def show_selection(self, selected_points):
+        self.selected_points = selected_points
+        return self.show_pair_plot()
 
-        self.traces = []
+    def show_pair_plot(self, component1=None, component2=None):
+        if not component2:
+            component1, component2, match_components = self._get_components()
+        else:
+            component1, component2, match_components = self._get_components(component1, component2)
 
+        self._component_traces = []
         multi_mode = component2 == 'ALL'
-        cluster_label_mode = not multi_mode and self._cluster_labels is not None
-
-        match_components = list(range(component1)) + list(range(component1 + 1, self._data.shape[0])) if multi_mode else [
-            component2]
+        cluster_label_mode = len(match_components) == 1 and self._cluster_labels is not None
 
         for component2 in match_components:
             # Default None - Any non-array value passed to selectedpoints kwarg indicates there is no selection present
-            selected_points = None
-            triggered = dash.callback_context.triggered
-            if '"type":"slice_graph"' in triggered[0]['prop_id'] and \
-                    'selectedData' in triggered[0]['prop_id'] and \
-                    triggered[0]['value'] is not None:
-                # selected data being None indicates that the user has selected data
-                # selected data 'points' being empty indicates the user has selected data outside of the region
-                selected_points = self._indexes_from_selection(triggered[0]['value'])
+            selected_points = self.selected_points
+            try:
+                x = self._data[component1]
+                y = self._data[component2]
+            except IndexError:
+                return self._update_figure()
 
-            x = self._data[component1]
-            y = self._data[component2]
-
-            self.traces.append(go.Scattergl(x=np.asarray(x.ravel()),
-                                            y=np.asarray(y.ravel()),
-                                            mode='markers',
-                                            marker={'color': 'rgba(0,0,0,0)'} if cluster_label_mode else None,
-                                            hoverinfo='skip' if cluster_label_mode else None,
-                                            showlegend=True if multi_mode else False,
-                                            selectedpoints=selected_points,
-                                            name=f'Component #{component2 + 1}' if multi_mode else None))
+            self._component_traces.append(go.Scattergl(x=np.asarray(x.ravel()),
+                                                       y=np.asarray(y.ravel()),
+                                                       mode='markers',
+                                                       marker={'color': 'rgba(0,0,0,0)'} if cluster_label_mode else None,
+                                                       hoverinfo='skip' if cluster_label_mode else None,
+                                                       showlegend=True if multi_mode else False,
+                                                       selectedpoints=selected_points,
+                                                       name=f'Component #{component2 + 1}' if multi_mode else None))
 
             if cluster_label_mode:
                 min_index = 0
@@ -200,14 +240,35 @@ class PairPlotGraph(dcc.Graph):
                                          name=name,
                                          mode='markers',
                                          selectedpoints=masked_selected_points)
-                    self.traces.append(trace)
+                    self._component_traces.append(trace)
                     min_index += np.count_nonzero(label_mask)
 
-        if 'clickData' in triggered[0]['prop_id']:
-            click_data = triggered[0]['value']
-            y_index = nearest_bin(click_data["points"][0]["y"], self._bounds[1], self._data.shape[1])
-            x_index = nearest_bin(click_data["points"][0]["x"], self._bounds[2], self._data.shape[2])
-            self._crosshair_index = np.ravel_multi_index((y_index, x_index), self._data.shape[1:])
+        return self._update_figure()
+
+    def _get_components(self, component1=None, component2=None):
+        if component1 is None:
+            component1 = dash.callback_context.states[
+                stringify_id(self.configuration_panel._decomposition_component_1.id) + '.value']
+        if component2 is None:
+            component2 = dash.callback_context.states[
+                stringify_id(self.configuration_panel._decomposition_component_2.id) + '.value']
+        if component1 is None or component2 is None:
+            raise PreventUpdate
+
+        multi_mode = component2 == 'ALL'
+        cluster_label_mode = not multi_mode and self._cluster_labels is not None
+
+        match_components = list(range(component1)) + list(range(component1 + 1, self._data.shape[0])) if multi_mode else [
+            component2]
+
+        return component1, component2, match_components
+
+    def update_selection(self, click_data):
+        component1, component2, match_components = self._get_components()
+
+        y_index = nearest_bin(click_data["points"][0]["y"], self._bounds[1], self._data.shape[1])
+        x_index = nearest_bin(click_data["points"][0]["x"], self._bounds[2], self._data.shape[2])
+        self._crosshair_index = np.ravel_multi_index((y_index, x_index), self._data.shape[1:])
 
         if self._crosshair_index is not None:
             x = [self._data[component1].ravel()[self._crosshair_index] for component2 in match_components]
@@ -215,10 +276,8 @@ class PairPlotGraph(dcc.Graph):
             self.crosshair_trace.x = x
             self.crosshair_trace.y = y
 
-        self.traces.append(self.crosshair_trace)
-
         self._xaxis_title = f'Component #{component1 + 1}'
-        self._yaxis_title = f'Other components' if multi_mode else f'Component #{component2 + 1}'
+        self._yaxis_title = f'Other components' if len(match_components) > 1 else f'Component #{component2 + 1}'
 
         return self._update_figure()
 
@@ -232,3 +291,12 @@ class PairPlotGraph(dcc.Graph):
             return {'display': 'block'}
         else:
             return {'display': 'none'}
+
+    def set_data(self, data):
+        self._data = data
+
+    def set_clustering(self, cluster_labels, label_names=None):
+        if label_names is None:
+            label_names = [phonetic_alphabet.read(chr(65+i)) for i in range(np.unique(cluster_labels).size)]
+        self._cluster_labels = cluster_labels
+        self._cluster_label_names = label_names
