b7195ebd62ec9ec842909dc62e7734a9c65835f5
ronpandolfi <ronpandolfi@gmail.com>
2021-11-11 15:46:27 -0800

    fix spectral correlation to operate slice-wise

--- irviz/quality_metrics/spectral_correlation.py
+++ irviz/quality_metrics/spectral_correlation.py
@@ -4,13 +4,15 @@ import einops
 __all__ = ['spectral_correlation']
 
 # TODO: change spectral mask to accept list of dicts representing masked regions
+from irviz.utils.mapper import selection_brackets_to_bool_array
 
 
-def spectral_correlation(data, eigen_spectra, components, mask, spectral_mask):
+def spectral_correlation(wavenumbers, data, eigen_spectra, components, mask, spectral_mask):
     """
 
     Parameters
     ----------
+    wavenumbers: spectral coordinates (Nwave,)
     data: IR data tensor (Nwav, Nx, Ny)
     eigen_spectra: Decomposition spectral eigenvectors (Ncomp, Nwav)
     components: Decomposition component amplitudes (Ncomp, Nx, Ny)
@@ -27,6 +29,7 @@ def spectral_correlation(data, eigen_spectra, components, mask, spectral_mask):
     x = einops.rearrange(data, "Nwav Nx Ny -> (Nx Ny) Nwav")[mask.astype(bool), :]
     components = einops.rearrange(components, 'Ncomp Nx Ny -> (Nx Ny) Ncomp')[mask.astype(bool), :]
     # spectral selection
+    spectral_mask = selection_brackets_to_bool_array(spectral_mask, wavenumbers)
     x = x[:, spectral_mask.astype(bool)]
     eigen_spectra = eigen_spectra[:, spectral_mask.astype(bool)]
 
@@ -38,10 +41,10 @@ def spectral_correlation(data, eigen_spectra, components, mask, spectral_mask):
     sxy = np.zeros(sy.shape)
 
     for i in range(x.shape[0]):  # could numba-ize this
-        y = np.dot(eigen_spectra.T, components[i])
+        y = np.dot(eigen_spectra.T, components[i])  # y is a reconstructed single spatial pixel slice, comparable to x[i]
         my[i] = np.mean(y)
         sy[i] = np.std(y)
-        sxy[i] = np.mean((x.T[i]-mx)*(y-my[i]))
+        sxy[i] = np.mean((x[i]-mx[i])*(y-my[i]))
 
     correlation = sxy/(sx*sy)
 
