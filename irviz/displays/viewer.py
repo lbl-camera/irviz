d264c39af851bab4d5c8b93884fc11560a061ac8
lchen23 <lchen43@gmail.com>
2022-06-04 18:05:26 -0700

    update deprecated dash html import

--- irviz/displays/viewer.py
+++ irviz/displays/viewer.py
@@ -6,7 +6,7 @@ from itertools import count
 
 import dash
 import dash_bootstrap_components as dbc
-import dash_html_components as html
+from dash import html
 import numpy as np
 from dash.dependencies import ALL, Input, Output
 
